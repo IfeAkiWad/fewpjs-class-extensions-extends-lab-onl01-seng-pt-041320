925d21919db7dc76f3dbc048d54ea5a2915f4457
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-12-03 21:22:46 -0600

    commit tree 007b52f250cbd6aa22f1aa23a508d69e2863c4a7

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+class Polygon {
+    // Polygon will accept an Array of integers as a parameter
+    constructor(array) {
+        // which will represent each side of a shape
+        this.sides = array
+    }
+
+    get countSides() {
+        return this.sides.length;
+    }
+
+    get perimeter() {
+        return this.sides.reduce((total, num) => {return total + num})
+    }
+}
